2838aba09d84bad1257c38b89f7468c349a73b47
peti <95631000+darthpeti@users.noreply.github.com>
2023-04-25 16:05:34 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,20 +5,21 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
-
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className="GUH CLASSNAME">
-        <h1 className="hero__title">Mvndicraft</h1>
-        <p className="hero__subtitle">Lords At War</p>
-        <div className="BUTTONS">
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className="ButtonsClass">
           <Link
             className="button button--secondary button--lg"
             to="/docs/welcome">
             Read The Docs
           </Link>
         </div>
+      </div>
     </header>
   );
 }
