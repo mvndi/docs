4ce7f6211eaf96e74af8e1939dd0f8feaa85657f
peti <95631000+darthpeti@users.noreply.github.com>
2023-04-25 15:47:08 +0300

    buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
+        <div className="BUTTONS">
           <Link
             className="button button--secondary button--lg"
             to="/docs/welcome">
