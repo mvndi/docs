af9364639be4faf43f4772cd15cc84f5dd12cede
peti <tudor_petrescu@yahoo.com>
2023-04-25 15:12:36 +0300

    chuddi craft

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -2,7 +2,39 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
-const FeatureList;
+const FeatureList = [{
+    title: 'Changing Vanilla',
+    Svg: require('@site/static/img/logo-old.svg').default,
+    description: (
+      <>
+        Mvndicraft does not require any mods for you to play,
+        but plugins change almost every aspect of Vanilla Minecraft
+      </>
+    ),
+  },
+  {
+    title: 'CVMING SQQN',
+    Svg: require('@site/static/img/logo-old.svg').default,
+    description: (
+      <>
+        Stay tuned for updates relating the official release 
+        in our Discord. Join here: discord.gg/2BP6h8Ssta
+      </>
+    ),
+  },
+  {
+    title: 'Towny on Eurasia',
+    Svg: require('@site/static/img/logo-old.svg').default,
+    description: (
+      <>
+        Make your settlement anywhere on the Middle East,
+        Europe or Africa. You can create a historically 
+        accurate nation and go to war with your enemies.
+      </>
+    ),
+  },
+
+];
 
 function Feature({Svg, title, description}) {
   return (
