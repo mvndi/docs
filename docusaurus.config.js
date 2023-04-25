66bb3eecfce63fd18a08f5570d1ae59eb1403ce1
peti <tudor_petrescu@yahoo.com>
2023-04-25 14:41:11 +0300

    the mvndi shaker

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,15 +11,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://mvndi.github.io',
+  url: 'https://mvndicraft.net',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'mvndi', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  organizationName: 'darthpeti', // Usually your GitHub org/user name.
+  projectName: 'mvndi-docs', // Usually your repo name.
   deploymentBranch: "main",
 
   onBrokenLinks: 'throw',
