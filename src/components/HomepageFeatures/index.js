eacf056271f327a70bd724be922667622a10f454
peti <tudor_petrescu@yahoo.com>
2023-04-25 15:06:09 +0300

    wahh wahh

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
-const FeatureList = [{}];
+const FeatureList;
 
 function Feature({Svg, title, description}) {
   return (
