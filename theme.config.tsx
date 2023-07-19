ede15e89f2d4234b6360555fa4d0a8212ae7a9f0
ewof <elwolf6@protonmail.com>
2023-07-19 10:44:10 -0400

    nextra rewrite

---
+++ theme.config.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const styles = {
+  container: {
+    paddingLeft: '10%'
+  },
+} as const;
+
+const config = {
+  head: (
+    <React.Fragment>
+      <meta name="description" content="Medieval geopolitics in Minecraft" />
+      <meta property="og:title" content="Medieval geopolitics in Minecraft" />
+      <meta property="og:description" content="Medieval geopolitics in Minecraft" />
+      <meta name="apple-mobile-web-app-title" content="Mvndicraft" />
+      <link rel="icon" type="image/png" href="/favicon.png"/>
+    </React.Fragment>
+  ),
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – Mvndicraft'
+    }
+  },
+  logo: <><img src="/favicon.png" alt="favicon" width="32"/> <span style={styles.container}>Mvndicraft</span></>,
+  chat: {
+    link: 'https://discord.gg/2BP6h8Ssta',
+  },
+  editLink: {
+    text: null,
+  },
+  feedback: {
+    content: null,
+  },
+  footer: {
+    text: "MVNDICRAFT @ 20223",
+  },
+}
+
+export default config
