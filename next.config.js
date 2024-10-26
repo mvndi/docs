0eb653e40a99e803d5dae723d980ef9886b3475d
tlm920 <timo@timomcgrath.com>
2024-10-25 20:27:06 -0400

    feat: don't cache webpack

--- next.config.js
+++ next.config.js
@@ -3,4 +3,20 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra()
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    if (config.cache && !dev) {
+      config.cache = Object.freeze({
+        type: 'memory',
+      })
+    }
+    // Important: return the modified config
+    return config
+  },
+}
+
+module.exports = withNextra(nextConfig)
